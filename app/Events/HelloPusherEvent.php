a12b11050ea26a1232a9e6aece6adcd75e92deca
Jonathan Machado <jonathan.mmachado@outlook.com>
2017-02-01 17:07:49 -0200

    update composer.json && add events/route

---
+++ app/Events/HelloPusherEvent.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Events;
+
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+
+class HelloPusherEvent implements ShouldBroadcast
+{
+    use SerializesModels;
+
+    /**
+    *
+    * Only (!) Public members will be serialized to JSON and sent to Pusher
+    *
+    **/
+     public $message;
+
+    /**
+     * Create a new event instance.
+     *
+     * @return void
+     */
+    public function __construct($message)
+    {
+        $this->message = $message;
+    }
+
+    /**
+     * Get the channels the event should broadcast on.
+     *
+     * @return Channel|array
+     */
+    public function broadcastOn()
+    {
+        return new Channel('my-channel');
+    }
+}
